1324983580a3e7a6af9a1c846bf9d3cca789411a
Yi Li <licds@bc.edu>
2020-02-10 15:36:04 -0500

    completed 5.4

---
+++ To Do List/ToDoDetailTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ToDoTableViewController.swift
+//  To Do List
+//
+//  Created by Yi Li on 2/10/20.
+//  Copyright © 2020 Yi Li. All rights reserved.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+    @IBOutlet weak var saveButtonPressed: UIBarButtonItem!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var noteView: UITextView!
+    
+    var toDoItem: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if toDoItem == nil {
+            toDoItem = ""
+        }
+        nameField.text = toDoItem
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        toDoItem = nameField.text
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+}
+   
