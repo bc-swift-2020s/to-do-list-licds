15c042ec1d9fb1880d57cfc5340940ce5e747680
Yi Li <licds@bc.edu>
2020-02-10 16:23:12 -0500

    Completed 5.6

---
+++ To Do List/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  To Do List
+//
+//  Created by Yi Li on 2/10/20.
+//  Copyright © 2020 Yi Li. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
